e52a7818d67af47186656960b582327ee06f4850
Jonathan <jon.ru92@googlemail.com>
2013-04-03 20:09:10 +0200

    Update 2

--- TableView.py
+++ TableView.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt4.QtGui import *
+from PyQt4.QtCore import *
 
 app = QApplication(sys.argv)
 
@@ -13,10 +14,11 @@ default, word = zip(*order)
 tableWidget = QTableWidget(0, len(order))
 tableWidget.resize(800,600)
 tableWidget.verticalHeader().setVisible(False)
-
 tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
+tableWidget.setGridStyle(Qt.PenStyle(0))
 tableWidget.setHorizontalHeaderLabels(word)
-tableWidget.horizontalHeader().setResizeMode(QHeaderView.Stretch)
+tableWidget.horizontalHeader().setResizeMode(QHeaderView.ResizeToContents)
 
 from FileReader import *
 
@@ -25,7 +27,14 @@ for f in listFiles('D:\Downloads', '*.mkv'):
     for i, c in enumerate(default):
         tableWidget.setItem(0, i, QTableWidgetItem(str(f[c])))
 
+tableWidget.setSortingEnabled(True)
 tableWidget.show()
 
+from os import system
+
+def command(item):
+    system("start " + item.text());
+
+tableWidget.itemClicked.connect(command)
 
 sys.exit(app.exec_())
