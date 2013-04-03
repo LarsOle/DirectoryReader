b612f150950118d3e5edb37ca91f4d80ae6b1d13
Lars Ole <larsole2000@gmail.com>
2013-04-03 18:17:54 +0200

    Test

--- TableView.py
+++ TableView.py
@@ -1,11 +1,12 @@
+import sys
 from PyQt4.QtGui import *
 
 app = QApplication(sys.argv)
 
 tableWidget = QTableWidget(0, 2)
 tableWidget.setHorizontalHeaderLabels(("Filename", "Filename"))
-tableWidget.setItem(0, 0, QTableViewWidgetItem("abc"))
+tableWidget.setItem(0, 0, QTableWidgetItem("abc"))
 tableWidget.show()
 
-import sys
+
 sys.exit(app.exec_())
