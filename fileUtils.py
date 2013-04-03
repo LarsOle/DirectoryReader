10d6e09d66e329ea805b0c18b2952f0cee902f93
LarsOle <larsole2000@gmail.com>
2013-04-04 00:35:55 +0300

    Update fileUtils.py

--- fileUtils.py
+++ fileUtils.py
@@ -2,9 +2,12 @@ from os import listdir, stat
 from os.path import join, isdir, basename
 from glob import glob
 
-def listFiles(path, pattern = '*'):
+#function to scan the directories for all files in the folder and in the subfolder
+def listFiles(path, pattern = ''):
 	files = []
 	for f in glob(join(path, pattern)):
+		if isdir(f):
+			break
 		fpath = join(path, f)
 		stats = stat(fpath)
 		files.append({'filename': basename(fpath)
