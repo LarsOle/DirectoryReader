a6e2430b83d136c6df59a2e9decac39a4977d617
Lars Ole <larsole2000@gmail.com>
2013-04-03 14:05:20 +0200

    Update
    Delete listFilesPattern() function and add default parameter to the
    listFiles() function.

--- FileReader.py
+++ FileReader.py
@@ -2,22 +2,12 @@ from os import listdir
 from os.path import join, isdir
 from glob import glob
 
-def listFilesPattern(path, pattern):
+def listFiles(path, pattern = '*'):
 	files = []
 	for f in glob(join(path, pattern)):
 		files.append(join(path, f))
 	for d in listdir(path):
 		fpath = join(path, d)
 		if isdir(fpath):
-			files.extend(listFilesPattern(fpath, pattern))
+			files.extend(listFiles(fpath, pattern))
 	return files
-
-def listFiles(path):
-    files = []
-    for f in listdir(path):
-       	fpath = join(path, f)
-       	if not isdir(fpath):
-           	files.append(fpath)
-       	else:
-           	files.extend(listFiles(fpath))
-    return files            
