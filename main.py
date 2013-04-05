df34d2d9e4661648e58d358e533e2e693c853212
Kanseki <jon.ru92@googlemail.com>
2013-04-05 19:50:16 +0200

    -

--- main.py
+++ main.py
@@ -1,40 +1,11 @@
-from PyQt4.QtGui import *
+from sys import argv, exit
 
-from customTableWidget import *
-from fileUtils import *
+from PyQt4 import QApplication
 
-from os.path import dirname, basename
-
-
-class MainWindow(QMainWindow):
-  def __init__(self, parent=None):
-    super(QMainWindow, self).__init__()
-    self.setMinimumSize(800, 800)
-
-    self.order = [('filename', 'Filename')
-            ,('filesize', 'Filesize')
-            ,('path', 'Path')
-            ]
-    self.tableView = customTableWidget(self.order)
-
-    self.inputBox = QLineEdit()
-    self.inputBox.textChanged.connect(self.search)
-    self.inputBox.setText('./*')
-    
-    self.vbox = QVBoxLayout()
-    self.vbox.addWidget(self.tableView)
-    self.vbox.addWidget(self.inputBox)
-    
-    self.setCentralWidget(QWidget())
-    self.centralWidget().setLayout(self.vbox)
-    
-  def search(self, path):
-    self.tableView.setItems(listFiles(dirname(path), basename(path)))    
-
-import sys
+from QMainWindow import QMainWindow
 
 if  __name__ ==  "__main__":
-  app = QApplication(sys.argv)
-  main = MainWindow()
-  main.show()
-  sys.exit(app.exec_())
+  app = QApplication(argv)
+  mainWindow = QMainWindow()
+  mainWindow.show()
+  exit(app.exec_())
