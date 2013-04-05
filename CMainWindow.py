5e9940dded3900ba438e60f9d5e343464002ea2a
Kanseki <jon.ru92@googlemail.com>
2013-04-05 19:51:48 +0200

    -

--- CMainWindow.py
+++ CMainWindow.py
@@ -1,1 +1,14 @@
- 
+from PyQt4 import QMainWindow
+
+from CWidget import CWidget
+
+class CMainWindow(QMainWindow):
+
+  def __init__(self, parent=None):
+    super(CMainWindow, self).__init__()
+    self.setMinimumSize(1280, 720)
+
+    self.cWidget = CWidget()
+    #self.cMenuBar = CMenuBar()
+
+    self.setCentralWidget(cWidget)
