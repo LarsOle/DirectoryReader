6bc1d75391567f520fb4cedffc6a52c0bbaa098b
Kanseki <jon.ru92@googlemail.com>
2013-04-05 21:37:44 +0200

    correction

--- CWidget.py
+++ CWidget.py
@@ -1,4 +1,5 @@
 from PyQt4.QtGui import QWidget, QTabWidget, QVBoxLayout
+
 from CTableWidget import CTableWidget
 
 class CWidget(QWidget):
@@ -6,10 +7,10 @@ class CWidget(QWidget):
     def __init__(self, parent=None):
         super(CWidget, self).__init__()
 
-        table1 = CTableWidget()
         self.cTabWidget = QTabWidget()
 
-        self.cTabWidget.addTab(table1, 1, "1. Liste")
+        self.table1 = CTableWidget()
+        self.cTabWidget.addTab(self.table1, "All Files")
 
         vbox = QVBoxLayout()
         vbox.addWidget(self.cTabWidget)
