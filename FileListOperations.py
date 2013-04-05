546213b4d29df1a7a8c113fe44ca0952f86eea9d
Kanseki <jon.ru92@googlemail.com>
2013-04-05 20:14:53 +0200

    new definition

--- FileListOperations.py
+++ FileListOperations.py
@@ -1,22 +1,18 @@
-import stat as stat2
 from os import listdir, stat
 from os.path import join, isfile, basename
-from fnmatch import fnmatch
+from stat import S_ISREG
 
-def listFiles(path, pattern = '*'):
-  ''' recursivly search for all files matching the given pattern
-      and returns all files beginning with the given path '''
+def listFiles(path):
   files = []
   for f in listdir(path):
     fpath = join(path, f)
     if isfile(fpath):
-      if fnmatch(f, pattern):
-        stats = stat(fpath)
-        if stat2.S_ISREG(stats.st_mode):
-          files.append({'filename': basename(fpath)
-            , 'path': fpath
-            , 'filesize': stats.st_size
-            })
+      stats = stat(fpath)
+      if S_ISREG(stats.st_mode):
+        files.append({'filename': basename(fpath)
+          , 'path': fpath
+          , 'filesize': stats.st_size
+          })
     else:
-        files.extend(listFiles(fpath, pattern))
+        files.extend(listFiles(fpath))
   return files
