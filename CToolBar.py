261bbd0943b5779a3ed9c100559c5ff1859fbbe7
Kanseki <jon.ru92@googlemail.com>
2013-04-05 23:36:22 +0200

    -

--- CToolBar.py
+++ CToolBar.py
@@ -1,6 +1,8 @@
 from PyQt4.QtGui import QToolBar, QAction, QIcon
 from PyQt4.QtCore import SIGNAL, SLOT
 
+from CDialogs import AddPathDialog, AddTabDialog
+
 class CToolBar(QToolBar):
 
     def __init__(self, parent=0):
@@ -9,12 +11,17 @@ class CToolBar(QToolBar):
 
         self.dir = QAction(QIcon('icons/dir.png'), 'Change directory', self)
         self.tab = QAction(QIcon('icons/tab.png'), 'Add tab', self)
-        self.exit = QAction(QIcon('icons/exit.png'), 'close applicatiob', self)
 
         self.addAction(self.dir)
         self.addAction(self.tab)
-        self.addAction(self.exit)
 
-        self.connect(self.dir, SIGNAL('triggered()'), SLOT('close()'))
-        self.connect(self.tab, SIGNAL('triggered()'), SLOT('close()'))
-        self.connect(self.exit, SIGNAL('triggered()'), SLOT('close()'))
+        self.connect(self.dir, SIGNAL('triggered()'), self.addPath)
+        self.connect(self.tab, SIGNAL('triggered()'), self.addTab)
+        
+    def addPath(self):
+        dialog = AddPathDialog()
+        dialog.exec()
+        
+    def addTab(self):
+        dialog = AddTabDialog()
+        dialog.exec()
